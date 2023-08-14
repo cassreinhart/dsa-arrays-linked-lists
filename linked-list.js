7237beb19770876d69d05cf70dcb1940c07934d6
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-08-14 10:45:00 -0500

    initial attempts

--- linked-list.js
+++ linked-list.js
@@ -21,55 +21,111 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
-
+    let newNode = new Node(val); //create a new node to store value
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-
+    let newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
-
+    let currentNode = this.head;
+    let deleteMe = this.tail;
+    while (currentNode.next) { //find the last node with next value that is not null
+      currentNode = currentNode.next
+    }
+    this.tail = currentNode.next
+    return deleteMe
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
-
+    let newHead = this.head.next;
+    let oldHead = this.head;
+    this.head = newHead;
+    return oldHead;
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-
+    const currentNode = this.head;
+    for (let i of this.length) {
+      currentNode = currentNode.next;
+      if (i === idx) {
+        return currentNode  
+      }
+    }
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-
+    const currentNode = this.head;
+    const newNode = new Node(val);
+    for (let i of this.length) {
+      if (i === idx) {
+        let nextNode = currentNode.next;
+        currentNode = newNode;
+        currentNode.next = nextNode;
+      }
+      currentNode = currentNode.next;
+    }
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
-
+    const currentNode = this.head;
+    const newNode = new Node(val);
+    for (let i of this.length) {
+      if (i === idx - 1) {
+        let nextNode = currentNode.next;
+        currentNode = newNode;
+        currentNode.next = nextNode;
+      }
+      currentNode = currentNode.next;
+    }
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
-
+    const currentNode = this.head;
+    for (let i of this.length) {
+      if (i === idx - 1) {
+        let deleteMe = currentNode.next;
+        let nextNode = currentNode.next.next;
+        currentNode.next = nextNode;
+        return deleteMe;
+      }
+      currentNode = currentNode.next
+    }
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
-    
+    let total = 0;
+    const currentNode = this.head;
+    for (let i of this.length) {
+      total += currentNode.val;
+      currentNode = currentNode.next;
+    }
+    return total;
   }
 }
 
